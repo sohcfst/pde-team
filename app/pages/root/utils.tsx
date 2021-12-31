6f4a8c22243974dffa8664fc0f5505c2a5575fed
cp <sockfasteat@gmail.com>
2021-12-31 09:01:35 -0800

    factor shit out into somewhat sensible places

---
+++ app/pages/root/utils.tsx
@@ -0,0 +1,25 @@
+export interface ImageWithParallaxConfig {
+  url: null | string;
+  x: string[];
+  y: string[];
+}
+
+export function shuffle(array: ImageWithParallaxConfig[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
--- app/root.tsx
+++ app/root.tsx
@@ -9,53 +9,17 @@ import { Head } from './pages/root/Head';
 import MemoryScroller from './components/MemoryScroller/MemoryScroller';
 import { Flex } from './components/Flex';
 import { NoiseBackground } from './components/Noise';
+import { parallaxConfig } from './pages/root/parallax.constants';
+import { ImageWithParallaxConfig, shuffle } from './pages/root/utils';
 
 export const meta: MetaFunction = () => {
   return { title: 'pde-team' };
 };
 
-interface ImageWithParallaxConfig {
-  url: null | string;
-  x: string[];
-  y: string[];
-}
-
 type LoaderData = { imageWithParallaxConfig: ImageWithParallaxConfig[] };
 
 export let loader: LoaderFunction = async () => {
   const images = await db.images.findMany();
-
-  const parallaxConfig = [
-    { y: ['400px', '0px'], x: ['-350px', '-350px'] }, // 1
-    { y: ['0px', '0px'], x: ['300px', '300px'] }, // 2
-    { y: ['200px', '0px'], x: ['-200px', '-200px'] }, // 3
-    { y: ['100px', '100px'], x: ['400px', '400px'] }, // 4
-    { y: ['400px', '200px'], x: ['50px', '50px'] }, // 5
-    { y: ['100px', '-800px'], x: ['-400px', '-400px'] }, // 6
-    { y: ['0px', '0px'], x: ['-300px', '-300px'] }, // 7
-    { y: ['100px', '-800px'], x: ['400px', '400px'] }, // 8
-    { y: ['100px', '-300px'], x: ['230px', '230px'] }, // 9
-    { y: ['150px', '-500px'], x: ['-400px', '-400px'] }, // 10
-    { y: ['0px', '0px'], x: ['-200px', '-200px'] }, // 11
-    { y: ['100px', '-800px'], x: ['330px', '330px'] }, // 12
-    { y: ['100px', '-500px'], x: ['-300px', '-300px'] }, // 13
-    { y: ['400px', '0px'], x: ['-350px', '-350px'] }, // 1
-    { y: ['0px', '0px'], x: ['300px', '300px'] }, // 2
-    { y: ['200px', '0px'], x: ['-200px', '-200px'] }, // 3
-    { y: ['100px', '100px'], x: ['400px', '400px'] }, // 4
-    { y: ['400px', '200px'], x: ['50px', '50px'] }, // 5
-    { y: ['100px', '-800px'], x: ['-400px', '-400px'] }, // 6
-    { y: ['0px', '0px'], x: ['-300px', '-300px'] }, // 7
-    { y: ['100px', '-800px'], x: ['400px', '400px'] }, // 8
-    { y: ['100px', '-300px'], x: ['230px', '230px'] }, // 9
-    { y: ['150px', '-500px'], x: ['-400px', '-400px'] }, // 10
-    { y: ['0px', '0px'], x: ['-200px', '-200px'] }, // 11
-    { y: ['100px', '-800px'], x: ['330px', '330px'] }, // 12
-    { y: ['100px', '-500px'], x: ['-300px', '-300px'] }, // 13
-    { y: ['150px', '-500px'], x: ['-400px', '-400px'] },
-    { y: ['100px', '100px'], x: ['400px', '400px'] },
-    { y: ['100px', '-800px'], x: ['-400px', '-400px'] },
-  ];
 
   const imageWithParallaxConfig = images.map((image, i) => {
     return {
@@ -64,26 +28,6 @@ export let loader: LoaderFunction = async () => {
       y: parallaxConfig[i].y,
     };
   });
-
-  function shuffle(array: ImageWithParallaxConfig[]) {
-    let currentIndex = array.length,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-
-    return array;
-  }
 
   const data: LoaderData = {
     imageWithParallaxConfig: shuffle(imageWithParallaxConfig),
